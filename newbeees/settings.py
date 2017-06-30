5a0596fd1ed98be5907c4da9bef7cdc58070986b
realousia <realousia@gmail.com>
2017-07-01 02:14:08 +0900

    pagination

--- newbeees/settings.py
+++ newbeees/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'poster',
+    'el_pagination',
 )
 
 MIDDLEWARE_CLASSES = (
--- poster/views.py
+++ poster/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from .models import Honeycomb as hc
 from .models import Honey as h
 from .models import Flower as f 
+from el_pagination.decorators import page_template
 
 # Create your views here.
-def index(request):
+@page_template('poster/entry_list.html')
+def index(request, 
+	template='poster/index.html',
+	extra_context=None):
 	hc_list = hc.objects.all()
-	h_list = h.objects.all()
-	return render(request, 'poster/index.html', {
-		'hc_list': hc_list,
+	h_list = h.objects.all().order_by('-id')
+	
+	context = {
+	    'hc_list': hc_list,
 		'h_list': h_list,
-	})
+	}
+	if extra_context is not None:
+		context.update(extra_context)
+	return render(request, template, context)
 
 def honey_list(request, pk):
 	hc_list = hc.objects.all()
@@ -21,14 +29,21 @@ def honey_list(request, pk):
 		'flower': flower,
 		'hc_list': hc_list,
 	})
-
-def honeycomb_detail(request, honeycomb):
+	
+@page_template('poster/entry_list.html')
+def honeycomb_detail(request, honeycomb, 
+	template='poster/honeycomb_detail.html', 
+	extra_context=None):
 	hc_list = hc.objects.all()
-	honey = h.objects.filter(honeycomb__name=honeycomb.upper())
-	return render(request, 'poster/honeycomb_detail.html', {
-		'honey': honey,
-		'hc_list': hc_list,
-	})
+	h_list = h.objects.filter(honeycomb__name=honeycomb.upper()).order_by('-id')
+	
+	context = {
+	    'hc_list': hc_list,
+		'h_list': h_list,
+	}
+	if extra_context is not None:
+		context.update(extra_context)
+	return render(request, template, context)
 
 def new_honey(request):
 	return render(request, 'poster/honey_form.html')
@@ -38,3 +53,4 @@ def about(request):
 	return render(request, 'poster/about.html', {
 		'hc_list': hc_list,	
 	})
+	
